a1a1535fef6d4fac74992723effaee941b1f5589
Gerson Rocha <gersonrocha9@gmail.com>
2023-01-28 11:57:24 -0300

    create swift project

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,63 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Gerson Rocha on 28/01/23.
+//
+
+import Foundation
+
+struct DailyScrum: Identifiable {
+    let id: UUID
+    var title: String
+    var attendees: [Attendee]
+    var lengthInMinutes: Int
+    var theme: Theme
+    
+    init(id: UUID, title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees.map { Attendee(id: UUID(), name: $0) }
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        init(id: UUID, name: String) {
+            self.id = id
+            self.name = name
+        }
+    }
+    
+    struct Data {
+        var title: String = ""
+        var attendees: [Attendee] = []
+        var lengthInMinutes: Double = 5
+        var theme: Theme = .seafoam
+    }
+    
+    var data: Data {
+        Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
+    }
+    
+    mutating func update(from data: Data) {
+        title = data.title
+        attendees = data.attendees
+        lengthInMinutes = Int(data.lengthInMinutes)
+        theme = data.theme
+    }
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(id: UUID(), title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(id: UUID(), title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(id: UUID(), title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,27 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Gerson Rocha on 28/01/23.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
